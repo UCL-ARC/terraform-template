b55a244aed51fb17fbe15a0af1db2fdc9b5cdf53
Ben Thomas <b.a.thomas@ucl.ac.uk>
2023-03-28 14:54:54 +0100

    Update style.rb
    Set max line length to 120

--- .github/workflows/style.rb
+++ .github/workflows/style.rb
@@ -1,7 +1,7 @@
 all
 
 # Don't enforce line length in code blocks
-rule 'MD013', :ignore_code_blocks => true, :tables => false
+rule 'MD013', :line_length => 120, :ignore_code_blocks => true, :tables => false
 
 # Don't force ordered lists with 1. 1. 1.
 rule 'MD029', :style => :ordered
